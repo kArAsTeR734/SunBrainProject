f252ec2d473bfaab4daebd1dd84928e598a3ae86
kArAsTeR734 <nikita.belyaev.05@mail.ru>
2026-05-02 15:32:23 +0300

    Navbar links added

--- SunBrainFrontend/src/shared/constants/navbarLinks.ts
+++ SunBrainFrontend/src/shared/constants/navbarLinks.ts
@@ -2,4 +2,5 @@ export const navLinks = [
   { label: 'О нас', href: '/' },
   { label: 'Каталог задач', href: '/student/catalog' },
   { label: 'Календарь', href: '/student/calendar' },
+  { label: 'Домашние задания', href: '/student/homework' },
 ];
